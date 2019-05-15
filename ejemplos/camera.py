602e36f789903e482fdca3b384bf0beefe5c0456
damianlancha <damianlancha@gmail.com>
2019-05-15 18:01:46 -0300

    Revert "no message"
    This reverts commit 69d7d4f4864374528cd05e727a2df3dc57d400e7.

--- ejemplos/camera.py
+++ ejemplos/camera.py
@@ -1,9 +1,8 @@
-import numpy as np  #instalar con: pip install numpy
-import cv2 #instalar con: pip install opencv-python
+import numpy as np
+import cv2
 
 #cap = cv2.VideoCapture('rtsp://<username_of_camera>:<password_of_camera@<ip_address_of_camera')
 cap = cv2.VideoCapture('http://admin:usher@irv.sytes.net:8081')
-#con cap = cv2.VideoCapture(0) se captura la web cam de la notebook
 
 while(True):
 
